8db3314aa14c1da42f0e25797fc42c20b883d3dc
Mike Wilson <mwilson@acesappraisal.com>
2016-01-03 17:13:40 -0500

    Initial Commit
    Signed-off-by: Mike Wilson <mwilson@acesappraisal.com>

---
+++ src/com/zaphod/AutoMateHelper/MapHelper.java
@@ -0,0 +1,36 @@
+package com.zaphod.AutoMateHelper;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.lang.Override;
+
+public class MapHelper extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onResume() {
+
+        super.onResume();
+        Intent launch = new Intent ("com.bitspice.automate.MAP_SCREEN");
+        sendBroadcast(launch);
+        finish();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    
+}
